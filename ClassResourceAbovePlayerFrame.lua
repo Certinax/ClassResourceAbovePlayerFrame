12fa3a8c755294491b4eeb7f47c0011232ea4eb2
Mathias Lund Ahrn <golvkaldt@gmail.com>
2024-05-18 23:20:31 +0200

    Addon to move class resource above player frame

---
+++ ClassResourceAbovePlayerFrame.lua
@@ -0,0 +1,8 @@
+local function AdjustFramePosition()
+  PlayerFrameBottomManageFramesContainer:ClearAllPoints();
+  PlayerFrameBottomManageFramesContainer:SetPoint("TOP", PlayerFrame, "TOP", 30, 10);
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame.SetScript("OnEvent", AdjustFramePosition)
---
+++ README.md
@@ -0,0 +1,10 @@
+# ClassResourceAbovePlayerFrame
+
+An addon that moves all class specific resource bar on top of the player frame.
+This will typically include resource bars as:
+- holy power
+- soul shards
+- chi bar
+- combo points
+- arcane orbs
+- eclipse
